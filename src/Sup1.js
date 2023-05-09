a3ab98e9bbebc9bd55e7d26ef29836a5776eeb3b
Nischal <kafle.nischal2021@gmail.com>
2023-05-09 20:25:08 +0545

    Last Commit

--- src/Sup1.js
+++ src/Sup1.js
@@ -1,57 +1,100 @@
 import React from 'react'
-import Footer from "./Footer";
 
 function Sup1() {
   return (
-    <>
-    <div id="main-support">
-    <div className="card small-card" id="support-staff">
-  <img src="../images1/Driver.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
-  <div class="card-body">
-    <h1 class="card-text">Krishna Pandey</h1>
-    <p id="new-font">Driver</p>
+    <div class="row row-cols-1 row-cols-md-5 g-4" id="bott" >
+    <div class="col">
+      <div class="card" id="border1">
+        <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+       
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card " id="border1">
+        <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+         
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card " id="border1">
+        <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+         
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" id="border1">
+        <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+         
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card " id="border1">
+        <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+        
+        </div>
+      </div>
+    </div>
+    <div class="col">
+    <div class="card" id="border1">
+      <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+     
+      </div>
+    </div>
+  </div>
+  <div class="col">
+  <div class="card" id="border1">
+    <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+   
+    </div>
   </div>
 </div>
-<div className="card small-card" id="support-staff">
-  <img src="../images1/Security.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
+<div class="col">
+<div class="card" id="border1">
+  <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
   <div class="card-body">
-    <h1 class="card-text">Ashish Khatri</h1>
-    <p id="new-font">Security</p>
-  </div>
-</div>
-<div className="card small-card" id="support-staff">
-  <img src="../images1/kritan.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
-  <div class="card-body">
-    <h1 class="card-text">Kritan Sitaula</h1>
-    <p id="new-font">Cook</p>
-  </div>
-</div>
-<div className="card small-card" id="support-staff">
-  <img src="../images1/ashish.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
-  <div class="card-body">
-    <h1 class="card-text">Ashish Khatri</h1>
-    <p id="new-font">Helper</p>
-  </div>
-</div>
-<div className="card small-card" id="support-staff">
-  <img src="../images1/Director4.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
-  <div class="card-body">
-    <h1 class="card-text">Dipesh Gyawali</h1>
-    <p id="new-font">Sweeper</p>
-  </div>
-</div>
-<div className="card small-card" id="support-staff">
-  <img src="../images1/Driver.jpg" className="card-img-top new-img" id="image-height" alt="..."/>
-  <div class="card-body">
-    <h1 class="card-text">Krishna Pandey</h1>
-    <p id="new-font">Driver</p>
+    <h5 class="card-title">Card title</h5>
+ 
   </div>
 </div>
 </div>
-
-
-</>
+<div class="col">
+<div class="card" id="border1">
+  <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+ 
+  </div>
+</div>
+</div>
+<div class="col">
+<div class="card" id="border1">
+  <img src="../images1/Driver.jpg" style={{height: "180px"}} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+ 
+  </div>
+</div>
+</div>
+  </div>
   )
 }
 
-export default Sup1;
+export default Sup1
